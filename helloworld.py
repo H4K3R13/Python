4fb682b4b5544db0ba533f509ac07a6b5acbb398
ARJUN C <50370450+ARJUNC13@users.noreply.github.com>
2021-08-10 10:44:56 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 10:46:44 2021
+
+@author: MADHUPRAJITHA
+"""
+
+print("Hello World")
---
+++ list.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 10:44:38 2021
+
+@author: MADHUPRAJITHA
+"""
+
+l=["I did it all",'4','love']
+for i in range(len(l)):
+    print(l[i])
